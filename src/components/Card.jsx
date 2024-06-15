35e96d9d9d1138cfab525bbd7da05f2b950c5ebd
mahen290 <[mahendraxx@gmail.com]>
2024-06-15 12:47:54 +0530

    change some new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,52 +1,16 @@
 import React from 'react';
 import './Card.css';
-import Credential from './Credential';
 
-// function Card( props ) {
-//   return (
-//     <div className = "card_container">
-//       <h2> { props.title } </h2>
-//       <img src = { props.source } />
-//       <p> { props.desc } </p>
-//       <button className ="new_btn"> { props.btn } </button>
-//     </div>
-//   );
-// }
-
-
-
-// function Card( props ) {
-
-//     console.log( props );
-
-//     return (
-        
-//         <div> 
-            
-//         </div>
-        
-//     );
-
-// }
-
-
-
-function Card( { title, source, desc, btn, index } ) {
+function Card( { title, source, desc, btn } ) {
 
     return (
         <div>
             <div className = "card_container">
-
                 <h2> { title } </h2>
                 <img src = { source } />
                 <p> { desc } </p>
                 <button className ="new_btn"> { btn } </button>
-
             </div>
-            <div> 
-                { index }
-                <Credential />  
-            </div> 
         </div>
     );
 }
--- src/components/Credential.jsx
+++ src/components/Credential.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Card from "./Card"
 function Credential() {
 
     const credentials = [
