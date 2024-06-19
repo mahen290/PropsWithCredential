d6bac218927c666e7464caeb86ea49d62b6fcbe0
mahen290 <[mahendraxx@gmail.com]>
2024-06-19 13:22:32 +0530

    Some Changes

--- src/components/Credential.jsx
+++ src/components/Credential.jsx
@@ -7,9 +7,9 @@ import Tiger from "../assets/Tiger.jpg";
 function Credential() {
     const credentials = [
 
-        { id : 1, title : "Car_1", src : Car, desc : "This is Red Car Model No. 21009", button : "Book - Now" },
+        { id : 1, title : "Car_1", src : Car, desc : "Red Car Model No. 21009", button : "Book - Now" },
 
-        { id : 2, title : "Car_2", src : MikeBird, desc : "This is Blue Car Of MikeBird Company Model No. 811", button : "Order - Now" },
+        { id : 2, title : "Car_2", src : MikeBird, desc : "Blue Car MikeBird Model No. 811", button : "Order - Now" },
 
         { id : 3, title : "Car_3", src : Tiger, desc : "This is Ferrari Car", button : "Booked" },
 
